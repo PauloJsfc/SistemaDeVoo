af572233d7531b9d0958b8be68e9f22add213436
Caio Silva <caioadrean@gmail.com>
2025-04-09 18:23:20 -0300

    Delete SistemaVooException.java
    commit errado

--- SistemaVooException.java
+++
@@ -1,8 +0,0 @@
-package dcx.ufpb.br.paulo.ferreira;
-
-public class SistemaVooException extends RuntimeException {
-    public SistemaVooException(String message) {
-        super(message);
-    }
-    //TODO: criar Exceptions específicas para cada caso
-}
